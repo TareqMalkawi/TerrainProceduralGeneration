52016e01bfb5de5db9d2a60db0c451c9b3e3157b
TareqMalkawi <tareqmalkawi17@gmail.com>
2022-11-03 00:29:28 +0300

    terrain generated procedurally with vertex color

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+    Color[] colors;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    public Gradient gradient;
+
+    float minHegith;
+    float maxHeight;
+
+    public int width = 256;
+    public int height = 256;
+
+    [Range(0.0f, 100.0f)] public float scale = 5.0f;
+    [Range(0.0f, 120.0f)] public float xOffset = 100.0f;
+    [Range(0.0f, 120.0f)] public float yOffset = 100.0f;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+       
+        //CreateShape();
+        //UpdateMesh();
+    }
+
+    private void Update()
+    {
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = GetHeight(x,z);
+                vertices[i] = new Vector3(x, y, z);
+
+                if(y > maxHeight)
+                    maxHeight = y;
+
+                if (y < minHegith)
+                    minHegith = y;
+
+                i++;
+            }
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        colors = new Color[vertices.Length];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float height = Mathf.InverseLerp(minHegith,maxHeight,vertices[i].y);
+                colors[i] = gradient.Evaluate(height);
+                i++;
+            }
+        }
+    }
+
+    private float GetHeight(int x, int y)
+    {
+        var xCoor = (float)x / width * scale + xOffset;
+        var yCoor = (float)y / height * scale + yOffset;
+
+        var sample = Mathf.PerlinNoise(xCoor, yCoor) * 8.0f;
+        return sample;
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+        mesh.RecalculateNormals();
+    }
+}
